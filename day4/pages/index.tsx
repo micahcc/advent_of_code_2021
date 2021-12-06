af506483046f3a3e2a661dc25e7929559fc27476
Micah Chambers <micahc.vt@gmail.com>
2021-12-06 00:59:37 -0800

    finish day 4

--- day4/pages/index.tsx
+++ day4/pages/index.tsx
@@ -1,11 +1,122 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import data from "./test_data.json";
+//import data from "./test_data.json";
+import data from "./full_data_1.json";
+
+class Board {
+  layout: number[][];
+  covered: boolean[][];
+  height: number;
+  width: number;
+
+  constructor(layout: number[][]) {
+    this.layout = layout;
+    this.height = layout.length;
+    this.width = layout[0].length;
+    this.covered = [];
+    for (let i = 0; i < this.height; ++i) {
+      this.covered.push([]);
+      for (let j = 0; j < this.width; ++j) {
+        this.covered[i].push(false);
+      }
+    }
+    console.log(layout);
+  }
+
+  mark(v: number) {
+    for (let i = 0; i < this.height; ++i) {
+      for (let j = 0; j < this.width; ++j) {
+        if (this.layout[i][j] === v) {
+          this.covered[i][j] = true;
+          console.log(`Covering ${v} at ${i},${j}`);
+        }
+      }
+    }
+
+    if (this.isComplete()) {
+      const partialScore = this.sumUnmarked();
+      console.log(this.covered);
+      console.log(this.layout);
+      return partialScore * v;
+    } else {
+      return 0;
+    }
+  }
+
+  sumUnmarked() {
+    let sum = 0;
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; ++j) {
+        if (!this.covered[i][j]) {
+          sum += this.layout[i][j];
+        }
+      }
+    }
+    return sum;
+  }
+
+  isComplete() {
+    for (let i = 0; i < this.height; i++) {
+      let complete = true;
+      for (let j = 0; j < this.width; ++j) {
+        if (!this.covered[i][j]) {
+          complete = false;
+          break;
+        }
+      }
+
+      if (complete) {
+        return true;
+      }
+    }
+
+    for (let j = 0; j < this.height; j++) {
+      let complete = true;
+      for (let i = 0; i < this.width; ++i) {
+        if (!this.covered[i][j]) {
+          complete = false;
+          break;
+        }
+      }
+
+      if (complete) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
 
 const Home: NextPage = () => {
-  console.log(data);
+  let boards: Board[] = [];
+  let complete: boolean[] = [];
+  let numWon = 0;
+  console.log(`Num Boards: ${data.boards.length}`);
+
+  /// make boards
+  for (let i = 0; i < data.boards.length; ++i) {
+    boards.push(new Board(data.boards[i]));
+    complete.push(false);
+  }
+
+  for (let i = 0; i < data.draws.length; ++i) {
+    console.log(`Drawing: ${data.draws[i]}`);
+    let winningScore = 0;
+    for (let b = 0; b < data.boards.length; ++b) {
+      if (complete[b]) {
+        continue;
+      }
+
+      winningScore = boards[b].mark(data.draws[i]);
+      if (winningScore > 0) {
+        console.log(`Win ${numWon}, board: ${b}, score: ${winningScore}`);
+        complete[b] = true;
+        numWon += 1;
+      }
+    }
+  }
 
   return (
     <div className={styles.container}>
@@ -21,18 +132,7 @@ const Home: NextPage = () => {
         </p>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
   );
 };
